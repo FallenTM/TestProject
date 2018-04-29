eceb1ef673be7d563693cffb4c8374f43c6bcdf0
Kyle <Kyle@192.168.0.13>
2018-04-29 16:24:36 -0400

    Updated Main

--- ReportPlus/src/me/fallenspring/reportplus/Main.java
+++ ReportPlus/src/me/fallenspring/reportplus/Main.java
@@ -1,6 +1,5 @@
 package me.fallenspring.reportplus;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.bukkit.Bukkit;
@@ -9,17 +8,21 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 	
 	private Inventory inv = Bukkit.createInventory(null, 27, "Reports Inventory");
 	
 	public void onEnable() {
 		Bukkit.getServer().getConsoleSender().sendMessage("[ReportPlus] has been enabled!");
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -53,11 +56,11 @@ public class Main extends JavaPlugin {
 					admins.sendMessage(ChatColor.RED + "-=[Reports Plus]");
 					ItemStack i = new ItemStack(Material.PAPER);
 					ItemMeta meta = i.getItemMeta();
-					meta.setDisplayName("Report");
+					meta.setDisplayName("Report, Click to Discard");
 					meta.setLore(Arrays.asList(ChatColor.RED + "Player: " + target.getName() + " has been reported for: " + message));
 					i.setItemMeta(meta);
 					
-					int amount = 0;
+					int amount = -1;
 					if (inv.getSize() > amount) {
 						amount++;
 						inv.setItem(amount, i);
@@ -65,10 +68,26 @@ public class Main extends JavaPlugin {
 					player.sendMessage(ChatColor.RED + "Report Submitted.");
 				}
 			}
-			if (cmd.getName().equalsIgnoreCase("reportgui")) {
-				player.openInventory(inv);
+		}
+		if (cmd.getName().equalsIgnoreCase("reportgui")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Sorry, but you cannot do that.");
+				return true;
 			}
+			Player player = (Player) sender;
+			player.openInventory(inv);
 		}
 		return false;
+	}
+	
+	@EventHandler
+	public void on(InventoryClickEvent e) {
+		if (e.getInventory().getName().equalsIgnoreCase(inv.getName())) {
+			if (e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("Report, Click to Discard")) {
+				e.setCancelled(true);
+				e.getCurrentItem().setType(Material.AIR);
+				e.getWhoClicked().sendMessage("You cleared a report.");
+			}
+		}
 	}
 }

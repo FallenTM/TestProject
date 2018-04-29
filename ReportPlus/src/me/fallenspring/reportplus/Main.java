a14f89b2a850f4baa5fd91f50a9df540b6a3de75
FallenTM <Kyle@192.168.0.13>
2018-04-29 12:48:56 -0400

    New Main

--- ReportPlus/src/me/fallenspring/reportplus/Main.java
+++ ReportPlus/src/me/fallenspring/reportplus/Main.java
@@ -1,7 +1,12 @@
 package me.fallenspring.reportplus;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		Bukkit.getServer().getConsoleSender().sendMessage("[ReportPlus] has been enabled!");
+	}
 
 }
